6d3ca2bdab07f02da7ca20d41be960670003d7b8
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-07-28 14:22:10 -0700

    schema and routes layed out

---
+++ models/workout.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now     
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "resistance"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Bicep Curl"
+        },
+        duration: {
+            type: Number,
+            unique: false
+        },
+        weight: {
+            type: Number,
+            unique: false
+        },
+        reps: {
+            type: Number,
+            unique: false
+        },
+        sets: {
+            type: Number,
+            unique: false
+        },
+        distance: {
+            type: Number,
+            unique: false
+        }
+    }]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
